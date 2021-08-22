6f475d5266cf08000d6399b50f0e679986e35ba6
efficacy38 <efficacy38@gmail.com>
2021-08-22 22:05:56 +0800

    change the home page carousel to image

--- src/screens/HomePage/Carousel.js
+++ src/screens/HomePage/Carousel.js
@@ -1,49 +1,58 @@
 import React, { Component } from "react";
-import { Text, View, Dimensions, StyleSheet } from "react-native";
+import { Text, View, Dimensions, StyleSheet, Image } from "react-native";
 
 import Carousel from "react-native-snap-carousel"; // Version can be specified in package.json
 
 import { scrollInterpolator, animatedStyles } from "../../utils/animations";
 
-const SLIDER_WIDTH = Dimensions.get("window").width  - 20;
+const SLIDER_WIDTH = Dimensions.get("window").width - 20;
 const SLIDER_HEIGHT = Dimensions.get("window").height;
 const ITEM_WIDTH = Math.round(SLIDER_WIDTH);
 const ITEM_HEIGHT = Math.round(SLIDER_HEIGHT / 5);
 
 export default class App extends Component {
-  state = {
-    index: 0,
-    entries: [0,1,2,3,4,5,6,7,8,9],
-  };
-
   constructor(props) {
     super(props);
+    this.state = {
+      index: 0,
+      entries: [
+        {
+          owner: "某某",
+          image: require("../../../assets/types-of-homes-hero.jpg"),
+        },
+        {
+          owner: "某某",
+          image: require("../../../assets/types-of-homes-hero.jpg"),
+        },
+      ],
+    };
     this._renderItem = this._renderItem.bind(this);
   }
 
   _renderItem({ item }) {
     return (
       <View style={styles.itemContainer}>
-        <Text style={styles.itemLabel}>{`Item ${item}`}</Text>
+        <Image source={item.image} resizeMode="cover" style={styles.image} />
+        <Text style={styles.itemLabel}>{`${item.owner}`}</Text>
       </View>
     );
   }
 
   render() {
     return (
-        <Carousel
-          ref={(c) => (this.carousel = c)}
-          data={this.state.entries}
-          renderItem={this._renderItem}
-          sliderWidth={SLIDER_WIDTH}
-          itemWidth={ITEM_WIDTH}
-          containerCustomStyle={styles.carouselContainer}
-          inactiveSlideShift={0}
-          onSnapToItem={(index) => this.setState({ index })}
-          scrollInterpolator={scrollInterpolator}
-          slideInterpolatedStyle={animatedStyles}
-          useScrollView={true}
-        />
+      <Carousel
+        ref={(c) => (this.carousel = c)}
+        data={this.state.entries}
+        renderItem={this._renderItem}
+        sliderWidth={SLIDER_WIDTH}
+        itemWidth={ITEM_WIDTH}
+        containerCustomStyle={styles.carouselContainer}
+        inactiveSlideShift={0}
+        onSnapToItem={(index) => this.setState({ index })}
+        scrollInterpolator={scrollInterpolator}
+        slideInterpolatedStyle={animatedStyles}
+        useScrollView={true}
+      />
     );
   }
 }
@@ -52,7 +61,7 @@ const styles = StyleSheet.create({
   carouselContainer: {
     marginTop: 20,
     height: ITEM_HEIGHT,
-    flexGrow: 0
+    flexGrow: 0,
   },
   itemContainer: {
     width: ITEM_WIDTH,
@@ -61,16 +70,24 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "dodgerblue",
     borderRadius: 10,
-
   },
   itemLabel: {
-    color: "white",
+    position: 'absolute',
+    textAlign: "left",
+    width: "100%",
+    color: "#000",
     fontSize: 24,
+    bottom: 0,
+    backgroundColor: "#c4c4c4",
+    paddingHorizontal: 5,
+    paddingVertical: 3,
+    backgroundColor: "rgba(196, 196, 196, 0.7)",
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
   },
-  counter: {
-    marginTop: 25,
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
   },
 });
